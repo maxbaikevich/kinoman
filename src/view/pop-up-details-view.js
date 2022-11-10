39828e85276399d0ee9a93628d8f8dc018c9f4ba
max.b <max.b@dev.izibook.ru>
2022-11-11 01:41:06 +0300

    Раскидал данные

--- src/view/pop-up-details-view.js
+++ src/view/pop-up-details-view.js
@@ -1,22 +1,43 @@
-export const popUpDetailsCreateTemplate = (movieData) => {
-  const  {title, poster, description,director} = movieData.filmInfo;
+import dayjs from 'dayjs';
+import {timeStempDuration} from '../utils';
 
+export const popUpDetailsCreateTemplate = (movieData) => {
+  const  {
+    title,
+    poster,
+    description,
+    director,
+    writers,
+    genre,
+    ageRating,
+    totalRating,
+    alternativeTitle,
+    actors,
+    release,
+    runtime
+  } = movieData.filmInfo;
+  const duration = timeStempDuration(runtime);
+
+  const createGenre = (genreData) => {
+    const res = genreData.map((el)=> (`<span class="film-details__genre">${el}</span>`));
+    return res.join(' ');
+  };
   return `<div class="film-details__info-wrap">
       <div class="film-details__poster">
         <img class="film-details__poster-img" src=${poster} alt="">
 
-        <p class="film-details__age">18+</p>
+        <p class="film-details__age">${ageRating}+</p>
       </div>
 
       <div class="film-details__info">
         <div class="film-details__info-head">
           <div class="film-details__title-wrap">
-            <h3 class="film-details__title">${title}</h3>
-            <p class="film-details__title-original">Original: The Great Flamarion</p>
+            <h3 class="film-details__title">${alternativeTitle}</h3>
+            <p class="film-details__title-original">Original: ${title}</p>
           </div>
 
           <div class="film-details__rating">
-            <p class="film-details__total-rating">8.9</p>
+            <p class="film-details__total-rating">${totalRating}</p>
           </div>
         </div>
 
@@ -27,33 +48,30 @@ export const popUpDetailsCreateTemplate = (movieData) => {
           </tr>
           <tr class="film-details__row">
             <td class="film-details__term">Writers</td>
-            <td class="film-details__cell">Anne Wigton, Heinz Herald, Richard Weil</td>
+            <td class="film-details__cell">${writers.join(', ')}</td>
           </tr>
           <tr class="film-details__row">
             <td class="film-details__term">Actors</td>
-            <td class="film-details__cell">Erich von Stroheim, Mary Beth Hughes, Dan Duryea</td>
+            <td class="film-details__cell">${actors.join(', ')}</td>
           </tr>
           <tr class="film-details__row">
             <td class="film-details__term">Release Date</td>
-            <td class="film-details__cell">30 March 1945</td>
+            <td class="film-details__cell">${dayjs(release.date).format('d MMMM YYYY')}</td>
           </tr>
           <tr class="film-details__row">
             <td class="film-details__term">Runtime</td>
-            <td class="film-details__cell">1h 18m</td>
+            <td class="film-details__cell">${duration.hours}h ${duration.minutes}m</td>
           </tr>
           <tr class="film-details__row">
             <td class="film-details__term">Country</td>
-            <td class="film-details__cell">USA</td>
+            <td class="film-details__cell">${release.releaseCountry}</td>
           </tr>
           <tr class="film-details__row">
             <td class="film-details__term">Genres</td>
             <td class="film-details__cell">
-              <span class="film-details__genre">Drama</span>
-              <span class="film-details__genre">Film-Noir</span>
-              <span class="film-details__genre">Mystery</span></td>
+              ${createGenre(genre)}
           </tr>
         </table>
-
         <p class="film-details__film-description">${description}</p>
       </div>
     </div>`;
