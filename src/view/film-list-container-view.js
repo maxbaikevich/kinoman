6d02f5745a74f33585f4fd25eed272db4264cb5a
max.b <max.b@dev.izibook.ru>
2022-11-14 22:35:20 +0300

    Ещё классы

--- src/view/film-list-container-view.js
+++ src/view/film-list-container-view.js
@@ -1,5 +1,24 @@
-import {elementCreate} from '../render';
+import {createElement} from '../render';
 const createFilmListContainerTemplate = () => (
-  `<div class="films-list__container"></div>`
-)
+  `<div class="films-list__container"></div>
+  `
+);
+
+export default  class filmListContainer {
+  #element;
+  get element () {
+    if(!this.#element) {
+      this.#element = createElement(this.template);
+    }
+    return this.#element;
+  }
+
+  get template() {
+    return createFilmListContainerTemplate();
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
 
--- src/view/show-more-view.js
+++ src/view/show-more-view.js
@@ -1,4 +1,23 @@
-export const showMoreBtnTemplate = () => (
+import {createElement} from '../render';
+const createShowMoreBtnTemplate = () => (
   `<button class="films-list__show-more">Show more</button>
   `
 );
+
+export default class showMoreBtn {
+  #element;
+  get element() {
+    if(!this.#element) {
+      this.#element = createElement(this.template);
+    }
+    return this.#element;
+  }
+
+  get template() {
+    return createShowMoreBtnTemplate();
+  }
+
+  removeElement() {
+    this.#element = null;
+  }
+}
